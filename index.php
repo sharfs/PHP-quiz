45c5126b6bdc99d67850a400884f0d7ed4f3b4ce
Sharfaa Anthony <sharfaasedick@gmail.com>
2019-03-25 20:18:42 +0200

    View this only

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="style.css" rel="stylesheet">
     <title>McQuiz</title>
 </head>
 <body>
@@ -109,6 +110,306 @@
 </div>
 </div>
 
+<div>
+<h3>6) What is 5+2?</h3>
+<div>
+    <input type="radio" name="question-6-answers" id="question-6-answers-A" value="A" />
+    <label for="question-6-answers-A">A) 3</label>
+</div>
+<div>
+    <input type="radio" name="question-6-answers" id="question-6-answers-B" value="B" />
+    <label for="question-6-answers-B">B) 7</label>
+</div>
+<div>
+    <input type="radio" name="question-6-answers" id="question-6-answers-C" value="C" />
+    <label for="question-6-answers-C">C) 10</label>
+</div>
+<div>
+    <input type="radio" name="question-6-answers" id="question-6-answers-D" value="D" />
+    <label for="question-6-answers-D">D) None of the above</label>
+</div>
+</div>
+
+<div>
+<h3>7) What is 9+3?</h3>
+<div>
+    <input type="radio" name="question-7-answers" id="question-7-answers-A" value="A" />
+    <label for="question-7-answers-A">A) 12</label>
+</div>
+<div>
+    <input type="radio" name="question-7-answers" id="question-7-answers-B" value="B" />
+    <label for="question-7-answers-B">B) 6</label>
+</div>
+<div>
+    <input type="radio" name="question-7-answers" id="question-7-answers-C" value="C" />
+    <label for="question-7-answers-C">C) 18</label>
+</div>
+<div>
+    <input type="radio" name="question-7-answers" id="question-7-answers-D" value="D" />
+    <label for="question-7-answers-D">D) None of the above</label>
+</div>
+</div>
+
+<div>
+<h3>8) What is 2+6?</h3>
+<div>
+    <input type="radio" name="question-8-answers" id="question-8-answers-A" value="A" />
+    <label for="question-8-answers-A">A) 5</label>
+</div>
+<div>
+    <input type="radio" name="question-8-answers" id="question-8-answers-B" value="B" />
+    <label for="question-8-answers-B">B) 10</label>
+</div>
+<div>
+    <input type="radio" name="question-8-answers" id="question-8-answers-C" value="C" />
+    <label for="question-8-answers-C">C) 11</label>
+</div>
+<div>
+    <input type="radio" name="question-8-answers" id="question-8-answers-D" value="D" />
+    <label for="question-8-answers-D">D) None of the above</label>
+</div>
+</div>
+
+<div>
+<h3>9) What is 9-7?</h3>
+<div>
+    <input type="radio" name="question-9-answers" id="question-9-answers-A" value="A" />
+    <label for="question-9-answers-A">A) 1</label>
+</div>
+<div>
+    <input type="radio" name="question-9-answers" id="question-9-answers-B" value="B" />
+    <label for="question-9-answers-B">B) 16</label>
+</div>
+<div>
+    <input type="radio" name="question-9-answers" id="question-9-answers-C" value="C" />
+    <label for="question-9-answers-C">C) 3</label>
+</div>
+<div>
+    <input type="radio" name="question-9-answers" id="question-9-answers-D" value="D" />
+    <label for="question-9-answers-D">D) None of the above</label>
+</div>
+</div>
+
+<div>
+<h3>10) What is 12-6?</h3>
+<div>
+    <input type="radio" name="question-10-answers" id="question-10-answers-A" value="A" />
+    <label for="question-10-answers-A">A) 5</label>
+</div>
+<div>
+    <input type="radio" name="question-10-answers" id="question-10-answers-B" value="B" />
+    <label for="question-10-answers-B">B) 18</label>
+</div>
+<div>
+    <input type="radio" name="question-10-answers" id="question-10-answers-C" value="C" />
+    <label for="question-10-answers-C">C) 8</label>
+</div>
+<div>
+    <input type="radio" name="question-10-answers" id="question-10-answers-D" value="D" />
+    <label for="question-10-answers-D">D) None of the above</label>
+</div>
+</div>
+
+<div>
+<h3>11) What is 11x3?</h3>
+<div>
+    <input type="radio" name="question-11-answers" id="question-11-answers-A" value="A" />
+    <label for="question-11-answers-A">A) 13</label>
+</div>
+<div>
+    <input type="radio" name="question-11-answers" id="question-11-answers-B" value="B" />
+    <label for="question-11-answers-B">B) 133</label>
+</div>
+<div>
+    <input type="radio" name="question-11-answers" id="question-11-answers-C" value="C" />
+    <label for="question-11-answers-C">C) 33</label>
+</div>
+<div>
+    <input type="radio" name="question-11-answers" id="question-11-answers-D" value="D" />
+    <label for="question-11-answers-D">D) None of the above</label>
+</div>
+</div>
+
+<div>
+<h3>12) What is 19-10?</h3>
+<div>
+    <input type="radio" name="question-12-answers" id="question-12-answers-A" value="A" />
+    <label for="question-12-answers-A">A) 9</label>
+</div>
+<div>
+    <input type="radio" name="question-12-answers" id="question-12-answers-B" value="B" />
+    <label for="question-12-answers-B">B) 29</label>
+</div>
+<div>
+    <input type="radio" name="question-12-answers" id="question-12-answers-C" value="C" />
+    <label for="question-12-answers-C">C) 6</label>
+</div>
+<div>
+    <input type="radio" name="question-12-answers" id="question-12-answers-D" value="D" />
+    <label for="question-12-answers-D">D) None of the above</label>
+</div>
+</div>
+
+<div>
+<h3>13) What is 20/4?</h3>
+<div>
+    <input type="radio" name="question-13-answers" id="question-13-answers-A" value="A" />
+    <label for="question-13-answers-A">A) 6</label>
+</div>
+<div>
+    <input type="radio" name="question-13-answers" id="question-13-answers-B" value="B" />
+    <label for="question-13-answers-B">B) 4</label>
+</div>
+<div>
+    <input type="radio" name="question-13-answers" id="question-13-answers-C" value="C" />
+    <label for="question-13-answers-C">C) 5</label>
+</div>
+<div>
+    <input type="radio" name="question-13-answers" id="question-13-answers-D" value="D" />
+    <label for="question-13-answers-D">D) None of the above</label>
+</div>
+</div>
+
+<div>
+<h3>14) What is 100/10?</h3>
+<div>
+    <input type="radio" name="question-14-answers" id="question-14-answers-A" value="A" />
+    <label for="question-14-answers-A">A) 100</label>
+</div>
+<div>
+    <input type="radio" name="question-14-answers" id="question-14-answers-B" value="B" />
+    <label for="question-14-answers-B">B) 10</label>
+</div>
+<div>
+    <input type="radio" name="question-14-answers" id="question-14-answers-C" value="C" />
+    <label for="question-14-answers-C">C) 1</label>
+</div>
+<div>
+    <input type="radio" name="question-14-answers" id="question-14-answers-D" value="D" />
+    <label for="question-14-answers-D">D) None of the above</label>
+</div>
+</div>
+
+<div>
+<h3>15) What is 5x5?</h3>
+<div>
+    <input type="radio" name="question-15-answers" id="question-15-answers-A" value="A" />
+    <label for="question-15-answers-A">A) 125</label>
+</div>
+<div>
+    <input type="radio" name="question-15-answers" id="question-15-answers-B" value="B" />
+    <label for="question-15-answers-B">B) 25</label>
+</div>
+<div>
+    <input type="radio" name="question-15-answers" id="question-15-answers-C" value="C" />
+    <label for="question-15-answers-C">C) 10</label>
+</div>
+<div>
+    <input type="radio" name="question-15-answers" id="question-15-answers-D" value="D" />
+    <label for="question-15-answers-D">D) None of the above</label>
+</div>
+</div>
+
+<div>
+<h3>16) What is 7/1?</h3>
+<div>
+    <input type="radio" name="question-16-answers" id="question-16-answers-A" value="A" />
+    <label for="question-16-answers-A">A) 7</label>
+</div>
+<div>
+    <input type="radio" name="question-16-answers" id="question-16-answers-B" value="B" />
+    <label for="question-16-answers-B">B) 1</label>
+</div>
+<div>
+    <input type="radio" name="question-16-answers" id="question-16-answers-C" value="C" />
+    <label for="question-16-answers-C">C) 0</label>
+</div>
+<div>
+    <input type="radio" name="question-16-answers" id="question-16-answers-D" value="D" />
+    <label for="question-16-answers-D">D) None of the above</label>
+</div>
+</div>
+
+<div>
+<h3>17) What is 8x8?</h3>
+<div>
+    <input type="radio" name="question-17-answers" id="question-17-answers-A" value="A" />
+    <label for="question-17-answers-A">A) 64</label>
+</div>
+<div>
+    <input type="radio" name="question-17-answers" id="question-17-answers-B" value="B" />
+    <label for="question-17-answers-B">B) 16</label>
+</div>
+<div>
+    <input type="radio" name="question-17-answers" id="question-17-answers-C" value="C" />
+    <label for="question-17-answers-C">C) 88</label>
+</div>
+<div>
+    <input type="radio" name="question-17-answers" id="question-17-answers-D" value="D" />
+    <label for="question-17-answers-D">D) None of the above</label>
+</div>
+</div>
+
+<div>
+<h3>18) What is 9+0?</h3>
+<div>
+    <input type="radio" name="question-18-answers" id="question-18-answers-A" value="A" />
+    <label for="question-18-answers-A">A) 0</label>
+</div>
+<div>
+    <input type="radio" name="question-18-answers" id="question-18-answers-B" value="B" />
+    <label for="question-18-answers-B">B) 10</label>
+</div>
+<div>
+    <input type="radio" name="question-18-answers" id="question-18-answers-C" value="C" />
+    <label for="question-18-answers-C">C) 9</label>
+</div>
+<div>
+    <input type="radio" name="question-18-answers" id="question-18-answers-D" value="D" />
+    <label for="question-18-answers-D">D) None of the above</label>
+</div>
+</div>
+
+<div>
+<h3>19) What is 4+6?</h3>
+<div>
+    <input type="radio" name="question-19-answers" id="question-19-answers-A" value="A" />
+    <label for="question-19-answers-A">A) 5</label>
+</div>
+<div>
+    <input type="radio" name="question-19-answers" id="question-19-answers-B" value="B" />
+    <label for="question-19-answers-B">B) 8</label>
+</div>
+<div>
+    <input type="radio" name="question-19-answers" id="question-19-answers-C" value="C" />
+    <label for="question-19-answers-C">C) 2</label>
+</div>
+<div>
+    <input type="radio" name="question-19-answers" id="question-19-answers-D" value="D" />
+    <label for="question-19-answers-D">D) None of the above</label>
+</div>
+</div>
+
+<div>
+<h3>20) What is 20-3?</h3>
+<div>
+    <input type="radio" name="question-20-answers" id="question-20-answers-A" value="A" />
+    <label for="question-20-answers-A">A) 23</label>
+</div>
+<div>
+    <input type="radio" name="question-20-answers" id="question-20-answers-B" value="B" />
+    <label for="question-20-answers-B">B) 17</label>
+</div>
+<div>
+    <input type="radio" name="question-20-answers" id="question-20-answers-C" value="C" />
+    <label for="question-20-answers-C">C) 203</label>
+</div>
+<div>
+    <input type="radio" name="question-20-answers" id="question-20-answers-D" value="D" />
+    <label for="question-20-answers-D">D) None of the above</label>
+</div>
+</div>
+
 <br>
 <h2><input type="submit" value="Submit Quiz" /></h2>
 
@@ -121,6 +422,21 @@
     $answer3 = $_POST["question-3-answers"];
     $answer4 = $_POST["question-4-answers"];
     $answer5 = $_POST["question-5-answers"];
+    $answer6 = $_POST["question-6-answers"];
+    $answer7 = $_POST["question-7-answers"];
+    $answer8 = $_POST["question-8-answers"];
+    $answer9 = $_POST["question-9-answers"];
+    $answer10 = $_POST["question-10-answers"];
+    $answer11 = $_POST["question-11-answers"];
+    $answer12 = $_POST["question-12-answers"];
+    $answer13 = $_POST["question-13-answers"];
+    $answer14 = $_POST["question-14-answers"];
+    $answer15 = $_POST["question-15-answers"];
+    $answer16 = $_POST["question-16-answers"];
+    $answer17 = $_POST["question-17-answers"];
+    $answer18 = $_POST["question-18-answers"];
+    $answer19 = $_POST["question-19-answers"];
+    $answer20 = $_POST["question-20-answers"];
 
     $totalCorrect = 0;
     
@@ -129,8 +445,23 @@
     if ($answer3 == "C") { $totalCorrect++; }
     if ($answer4 == "D") { $totalCorrect++; }
     if ($answer5 == "B") { $totalCorrect++; }
+    if ($answer6 == "B") { $totalCorrect++; }
+    if ($answer7 == "A") { $totalCorrect++; }
+    if ($answer8 == "D") { $totalCorrect++; }
+    if ($answer9 == "D") { $totalCorrect++; }
+    if ($answer10 == "D") { $totalCorrect++; }
+    if ($answer11 == "C") { $totalCorrect++; }
+    if ($answer12 == "A") { $totalCorrect++; }
+    if ($answer13 == "C") { $totalCorrect++; }
+    if ($answer14 == "B") { $totalCorrect++; }
+    if ($answer15 == "B") { $totalCorrect++; }
+    if ($answer16 == "A") { $totalCorrect++; }
+    if ($answer17 == "A") { $totalCorrect++; }
+    if ($answer18 == "C") { $totalCorrect++; }
+    if ($answer19 == "D") { $totalCorrect++; }
+    if ($answer20 == "B") { $totalCorrect++; }
     
-    echo "<h1>" . "<div id='results'>$totalCorrect / 5 correct</div>" . "</h1>";
+    echo "<h1>" . "<div id='results'>$totalCorrect / 20 correct</div>" . "</h1>";
     
 ?>
 </body>
