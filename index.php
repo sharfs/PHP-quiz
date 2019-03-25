a87a4be41b98209e0f330027f511512acb66088a
Sharfaa Anthony <sharfaasedick@gmail.com>
2019-03-25 17:28:12 +0200

    Updated Quiz

--- index.php
+++ index.php
@@ -4,55 +4,134 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link href="style.css" ref="stylesheet">
     <title>McQuiz</title>
 </head>
 <body>
+<form action="index.php" method="post" id="quiz">
+
+<div>
+<h3>1) What is 1+1?</h3>
+<div>
+    <input type="radio" name="question-1-answers" id="question-1-answers-A" value="A" />
+    <label for="question-1-answers-A">A) 4</label>
+</div>
+<div>
+    <input type="radio" name="question-1-answers" id="question-1-answers-B" value="B" />
+    <label for="question-1-answers-B">B) 2</label>
+</div>
+<div>
+    <input type="radio" name="question-1-answers" id="question-1-answers-C" value="C" />
+    <label for="question-1-answers-C">C) 1</label>
+</div>
+<div>
+    <input type="radio" name="question-1-answers" id="question-1-answers-D" value="D" />
+    <label for="question-1-answers-D">D) None of the above</label>
+</div>
+</div>
+
+<div>
+<h3>2) What is 2+1?</h3>
+<div>
+    <input type="radio" name="question-2-answers" id="question-2-answers-A" value="A" />
+    <label for="question-2-answers-A">A) 3</label>
+</div>
+<div>
+    <input type="radio" name="question-2-answers" id="question-2-answers-B" value="B" />
+    <label for="question-2-answers-B">B) 1</label>
+</div>
+<div>
+    <input type="radio" name="question-2-answers" id="question-2-answers-C" value="C" />
+    <label for="question-2-answers-C">C) 2</label>
+</div>
+<div>
+    <input type="radio" name="question-2-answers" id="question-2-answers-D" value="D" />
+    <label for="question-2-answers-D">D) None of the above</label>
+</div>
+</div>
+
+<div>
+<h3>3) What is 3x5?</h3>
+<div>
+    <input type="radio" name="question-3-answers" id="question-3-answers-A" value="A" />
+    <label for="question-3-answers-A">A) 2</label>
+</div>
+<div>
+    <input type="radio" name="question-3-answers" id="question-3-answers-B" value="B" />
+    <label for="question-3-answers-B">B) 8</label>
+</div>
+<div>
+    <input type="radio" name="question-3-answers" id="question-3-answers-C" value="C" />
+    <label for="question-3-answers-C">C) 15</label>
+</div>
+<div>
+    <input type="radio" name="question-3-answers" id="question-3-answers-D" value="D" />
+    <label for="question-3-answers-D">D) None of the above</label>
+</div>
+</div>
+
+<div>
+<h3>4) What is 16/2?</h3>
+<div>
+    <input type="radio" name="question-4-answers" id="question-4-answers-A" value="A" />
+    <label for="question-4-answers-A">A) 4</label>
+</div>
+<div>
+    <input type="radio" name="question-4-answers" id="question-4-answers-B" value="B" />
+    <label for="question-4-answers-B">B) 14</label>
+</div>
+<div>
+    <input type="radio" name="question-4-answers" id="question-4-answers-C" value="C" />
+    <label for="question-4-answers-C">C) 32</label>
+</div>
+<div>
+    <input type="radio" name="question-4-answers" id="question-4-answers-D" value="D" />
+    <label for="question-4-answers-D">D) None of the above</label>
+</div>
+</div>
+
+<div>
+<h3>5) What is 5+5?</h3>
+<div>
+    <input type="radio" name="question-5-answers" id="question-5-answers-A" value="A" />
+    <label for="question-5-answers-A">A) 5</label>
+</div>
+<div>
+    <input type="radio" name="question-5-answers" id="question-5-answers-B" value="B" />
+    <label for="question-5-answers-B">B) 10</label>
+</div>
+<div>
+    <input type="radio" name="question-5-answers" id="question-5-answers-C" value="C" />
+    <label for="question-5-answers-C">C) 11</label>
+</div>
+<div>
+    <input type="radio" name="question-5-answers" id="question-5-answers-D" value="D" />
+    <label for="question-5-answers-D">D) None of the above</label>
+</div>
+</div>
+
+<br>
+<h2><input type="submit" value="Submit Quiz" /></h2>
+
+</form>
+
+<?php
     
-    <?php
-    if($_POST){
-        $x=0;
+    $answer1 = $_POST["question-1-answers"];
+    $answer2 = $_POST["question-2-answers"];
+    $answer3 = $_POST["question-3-answers"];
+    $answer4 = $_POST["question-4-answers"];
+    $answer5 = $_POST["question-5-answers"];
 
-    if(isset($_POST['language']) && $_POST['language']==="humans") {
-        $x++;
-    }
-
-    if(isset($_POST['source']) && $_POST['source']==="statements") {
-        $x++;
-    }
-
-    if(isset($_POST['open']) && $_POST['open']==="false") {
-        $x++;
-    }
-        echo "Your score is $x";
-    }
-
-    ?>
-
-    <main role="main" class="container">
-    <br>
+    $totalCorrect = 0;
     
-    <form action= "index.php" method= "post">
-    <form role="form" action=<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?> 
-    method="post">
+    if ($answer1 == "B") { $totalCorrect++; }
+    if ($answer2 == "A") { $totalCorrect++; }
+    if ($answer3 == "C") { $totalCorrect++; }
+    if ($answer4 == "D") { $totalCorrect++; }
+    if ($answer5 == "B") { $totalCorrect++; }
     
-    What is 1+1?
-    <br>
-    <input type="radio" name="language" value="1">1<br>
-    <input type="radio" name="language" value="2">2<br>
-    <input type="radio" name="language" value="4">4
-    <br>
-    <br>
-
-    What is 4+3?
-    <br>
-    <input type="radio" name="language" value="7">7<br>
-    <input type="radio" name="language" value="5">5<br>
-    <input type="radio" name="language" value="6">6
-    <br>
-    <br>
-
-    <button type="submit">Submit</button>
-
+    echo "<h1>" . "<div id='results'>$totalCorrect / 5 correct</div>" . "</h1>";
+    
+?>
 </body>
 </html>
--- prac.php
+++ prac.php
@@ -22,115 +22,115 @@ $questions = array(
     <input type="radio" name="q2" value="A" />Ans 1<br>
     <input type="radio" name="q2" value="B" />Ans 2<br>
     <input type="radio" name="q2" value="C" />Ans 3<br>
-    <input type="radio" name="q2" value="D" />Ans 4<br>`,
+    <input type="radio" name="q2" value="D" />Ans 4`,
     `
     <h4>Question 3</h4>
     <input type="radio" name="q3" value="A" />Ans 1<br>
     <input type="radio" name="q3" value="B" />Ans 2<br>
     <input type="radio" name="q3" value="C" />Ans 3<br>
-    <input type="radio" name="q3" value="D" />Ans 4<br>`,
+    <input type="radio" name="q3" value="D" />Ans 4`,
     `
     <h4>Question 4</h4>
     <input type="radio" name="q4" value="A" />Ans 1<br>
     <input type="radio" name="q4" value="B" />Ans 2<br>
     <input type="radio" name="q4" value="C" />Ans 3<br>
-    <input type="radio" name="q4" value="D" />Ans 4<br>`,
+    <input type="radio" name="q4" value="D" />Ans 4`,
     `
     <h4>Question 5</h4>
     <input type="radio" name="q5" value="A" />Ans 1<br>
     <input type="radio" name="q5" value="B" />Ans 2<br>
     <input type="radio" name="q5" value="C" />Ans 3<br>
-    <input type="radio" name="q5" value="D" />Ans 4<br>`,
+    <input type="radio" name="q5" value="D" />Ans 4`,
     `
     <h4>Question 6</h4>
     <input type="radio" name="q6" value="A" />Ans 1<br>
     <input type="radio" name="q6" value="B" />Ans 2<br>
     <input type="radio" name="q6" value="C" />Ans 3<br>
-    <input type="radio" name="q6" value="D" />Ans 4<br>`,
+    <input type="radio" name="q6" value="D" />Ans 4`,
     `
     <h4>Question 7</h4>
     <input type="radio" name="q7" value="A" />Ans 1<br>
     <input type="radio" name="q7" value="B" />Ans 2<br>
     <input type="radio" name="q7" value="C" />Ans 3<br>
-    <input type="radio" name="q7" value="D" />Ans 4<br>`,
+    <input type="radio" name="q7" value="D" />Ans 4`,
     `
     <h4>Question 8</h4>
     <input type="radio" name="q8" value="A" />Ans 1<br>
     <input type="radio" name="q8" value="B" />Ans 2<br>
     <input type="radio" name="q8" value="C" />Ans 3<br>
-    <input type="radio" name="q8" value="D" />Ans 4<br>`,
+    <input type="radio" name="q8" value="D" />Ans 4`,
     `
     <h4>Question 9</h4>
     <input type="radio" name="q9" value="A" />Ans 1<br>
     <input type="radio" name="q9" value="B" />Ans 2<br>
     <input type="radio" name="q9" value="C" />Ans 3<br>
-    <input type="radio" name="q9" value="D" />Ans 4<br>`,
+    <input type="radio" name="q9" value="D" />Ans 4`,
     `
     <h4>Question 10</h4>
     <input type="radio" name="q10" value="A" />Ans 1<br>
     <input type="radio" name="q10" value="B" />Ans 2<br>
     <input type="radio" name="q10" value="C" />Ans 3<br>
-    <input type="radio" name="q10" value="D" />Ans 4<br>`,
+    <input type="radio" name="q10" value="D" />Ans 4`,
     `
     <h4>Question 11</h4>
     <input type="radio" name="q11" value="A" />Ans 1<br>
     <input type="radio" name="q11" value="B" />Ans 2<br>
     <input type="radio" name="q11" value="C" />Ans 3<br>
-    <input type="radio" name="q11" value="D" />Ans 4<br>`,
+    <input type="radio" name="q11" value="D" />Ans 4`,
     `
     <h4>Question 12</h4>
     <input type="radio" name="q12" value="A" />Ans 1<br>
     <input type="radio" name="q12" value="B" />Ans 2<br>
     <input type="radio" name="q12" value="C" />Ans 3<br>
-    <input type="radio" name="q12" value="D" />Ans 4<br>`,
+    <input type="radio" name="q12" value="D" />Ans 4`,
     `
     <h4>Question 13</h4>
     <input type="radio" name="q13" value="A" />Ans 1<br>
     <input type="radio" name="q13" value="B" />Ans 2<br>
     <input type="radio" name="q13" value="C" />Ans 3<br>
-    <input type="radio" name="q13" value="D" />Ans 4<br>`,
+    <input type="radio" name="q13" value="D" />Ans 4`,
     `
     <h4>Question 14</h4>
     <input type="radio" name="q14" value="A" />Ans 1<br>
     <input type="radio" name="q14" value="B" />Ans 2<br>
     <input type="radio" name="q14" value="C" />Ans 3<br>
-    <input type="radio" name="q14" value="D" />Ans 4<br>`,
+    <input type="radio" name="q14" value="D" />Ans 4`,
     `
     <h4>Question 15</h4>
     <input type="radio" name="q15" value="A" />Ans 1<br>
     <input type="radio" name="q15" value="B" />Ans 2<br>
     <input type="radio" name="q15" value="C" />Ans 3<br>
-    <input type="radio" name="q15" value="D" />Ans 4<br>`,
+    <input type="radio" name="q15" value="D" />Ans 4`,
     `
     <h4>Question 16</h4>
     <input type="radio" name="q16" value="A" />Ans 1<br>
     <input type="radio" name="q16" value="B" />Ans 2<br>
     <input type="radio" name="q16" value="C" />Ans 3<br>
-    <input type="radio" name="q16" value="D" />Ans 4<br>`,
+    <input type="radio" name="q16" value="D" />Ans 4`,
     `
     <h4>Question 17</h4>
     <input type="radio" name="q17" value="A" />Ans 1<br>
     <input type="radio" name="q17" value="B" />Ans 2<br>
     <input type="radio" name="q17" value="C" />Ans 3<br>
-    <input type="radio" name="q17" value="D" />Ans 4<br>`,
+    <input type="radio" name="q17" value="D" />Ans 4`,
     `
     <h4>Question 18</h4>
     <input type="radio" name="q18" value="A" />Ans 1<br>
     <input type="radio" name="q18" value="B" />Ans 2<br>
     <input type="radio" name="q18" value="C" />Ans 3<br>
-    <input type="radio" name="q18" value="D" />Ans 4<br>`,
+    <input type="radio" name="q18" value="D" />Ans 4`,
     `
     <h4>Question 19</h4>
     <input type="radio" name="q19" value="A" />Ans 1<br>
     <input type="radio" name="q19" value="B" />Ans 2<br>
     <input type="radio" name="q19" value="C" />Ans 3<br>
-    <input type="radio" name="q19" value="D" />Ans 4<br>`,
+    <input type="radio" name="q19" value="D" />Ans 4`,
     `
     <h4>Question 20</h4>
     <input type="radio" name="q20" value="A" />Ans 1<br>
     <input type="radio" name="q20" value="B" />Ans 2<br>
     <input type="radio" name="q20" value="C" />Ans 3<br>
-    <input type="radio" name="q20" value="D" />Ans 4<br>`,
+    <input type="radio" name="q20" value="D" />Ans 4`,
 );
 ?>
 
@@ -160,6 +160,63 @@ if(!empty($_POST)){
 
 }
 ?>
+
+</body>
+</html>
+
+
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="style.css" ref="stylesheet">
+    <title>McQuiz</title>
+</head>
+<body>
+    
+    <?php
+    if($_POST){
+        $x=0;
+
+    if(isset($_POST['language']) && $_POST['language']==="humans") {
+        $x++;
+    }
+
+    if(isset($_POST['source']) && $_POST['source']==="statements") {
+        $x++;
+    }
+
+    if(isset($_POST['open']) && $_POST['open']==="false") {
+        $x++;
+    }
+        echo "Your score is $x";
+    }
+
+    ?>
+
+    <main role="main" class="container">
+    <br>
+    
+    <form action= "index.php" method= "post">
+    <form role="form" action=<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?> 
+    method="post">
+    
+    What is 1+1?
+    <br>
+    <input type="radio" name="quesOne" value="1">1<br>
+    <input type="radio" name="quesOne" value="2">2<br>
+    <input type="radio" name="quesOne" value="6">6<br>
+    <input type="radio" name="quesOne"
+        <?php if (isset($quesOne) && $quesOne=="2") echo "checked";?>
+            value="2">2<br>
+
+    <button type="submit">Submit</button>
 
 </body>
 </html>
